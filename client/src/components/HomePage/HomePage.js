fa7be5f50a5cb16a921566f9e5346b3d7505b65d
devharp <hsingh.161297@gmail.com>
2022-03-10 22:23:52 +0530

    devharp: de-sassifying process, HomePage done

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -4,39 +4,42 @@ import Header from "../UI/Header/Header";
 import "./HomePage.scss";
 
 const HomePage = () => {
-  
+
   return (
-    <div className="home-page">
+    <div className="home-page d-flex flex-column" style={{height: '100vh'}}>
       <Header />
-      <div className="body">
-        <div className="left-side">
-          <div className="content">
-            <h2>We Connect video meetings. Now you can meet together.</h2>
+      <div className="d-flex align-items-center justify-content-around flex-grow-1">
+        <div className="" style={{width: '32vw'}}>
+          <div className="">
+            <div className="h1" style={{color: 'rgba(60, 60, 60, 1)', maxWidth: '30vw'}}>We Connect video meetings. Now you can meet together.</div>
             <p>
-             We can built this application for educational purpose.
+              We can built this application for educational purpose.
             </p>
-            <div className="action-btn">
-              <button className="btn green">
-                <FontAwesomeIcon className="icon-block" icon={faVideo} />
-                New Meeting
-              </button>
-              <div className="input-block">
-                <div className="input-section">
-                  <FontAwesomeIcon className="icon-block" icon={faKeyboard} />
-                  <input placeholder="Enter a code or link" />
-                </div>
-                <button className="btn no-bg">Join</button>
+            <div className="d-flex align-items-center justify-content-between">
+              <div>
+                <button className="btn btn-primary">
+                  <FontAwesomeIcon className="me-2" icon={faVideo} />
+                  New Meeting
+                </button>
+              </div>
+              <div className="d-flex align-items-center">
+                <FontAwesomeIcon className="me-2" icon={faKeyboard} />
+                <input className="form-control" placeholder="Enter a code or link" />
+              </div>
+              <div>
+                <button className="btn btn-primary">Join</button>
               </div>
             </div>
           </div>
-          <div className="help-text">
-            <a href="">Learn more</a> about We Connect
+          <div className="pt-4 mt-5" style={{borderTop: '1px solid #ddd'}}>
+            <a href="" className="text-primary" style={{textDecoration: 'none'}}>Learn more</a> about We Connect
           </div>
         </div>
-        <div className="right-side">
-          <div className="content d-none d-lg-block">
-            <img src="https://www.gstatic.com/meet/google_meet_marketing_ongoing_meeting_grid_427cbb32d746b1d0133b898b50115e96.jpg" />
-            
+        <div className="shadow">
+          <div className="content p-4" style={{border: '1px solid #eee', borderRadius: '5px'}}>
+            {/* <img src="https://www.gstatic.com/meet/google_meet_marketing_ongoing_meeting_grid_427cbb32d746b1d0133b898b50115e96.jpg" /> */}
+            <img style={{width: '40vw'}} src="https://www.gstatic.com/meet/google_meet_marketing_ongoing_meeting_grid_427cbb32d746b1d0133b898b50115e96.jpg" />
+
           </div>
         </div>
       </div>
